63acc37a4dd59797411deb73791b147ae48838ce
nijataliyev1 <nicat3699@gmail.com>
2023-11-16 23:25:46 +0400

    v 1.0

---
+++ src/Companents/Aside.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Aside.css';
+
+const Aside = () => {
+  return (
+    <div className='Aside'>
+      <p>Hello World</p>
+    </div>
+  )
+}
+
+export default Aside
---
+++ src/Companents/Datas.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Datas.css";
+
+const Datas = () => {
+  return (
+    <div className='Datas'>
+      <p>Test</p>
+    </div>
+  )
+}
+
+export default Datas
