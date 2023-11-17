18326659d14719c6d864c11641a058cb066c2b08
nijataliyev1 <nicat3699@gmail.com>
2023-11-17 14:58:55 +0400

    v 2.0

--- src/Companents/Aside.jsx
+++ src/Companents/Aside.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Aside.css';
+import image from '../images/cv_photo.jpg'
 
 const Aside = () => {
   return (
     <div className='Aside'>
-      <p>Hello World</p>
+      <div className="image">
+        <img src={image} alt="photo" className='photo' />
+      </div>
+      <div className="contact">
+        <h1>Əlaqə</h1>
+        <ul>
+          <li>Ünvan: Bakı şəhəri Xəzər ray. Babayev küç.</li>
+          <li>Email: nicat3699@gmail.com</li>
+          <li>Mobil: +994507711250</li>
+        </ul>
+      </div>
+      <div className="languages">
+        <h1>Dil biliyi</h1>
+        <ul>
+          <li>İngilis dili B1</li>
+        </ul>
+      </div>
     </div>
   )
 }
--- src/Companents/Datas.jsx
+++ src/Companents/Datas.jsx
@@ -4,7 +4,41 @@ import "./Datas.css";
 const Datas = () => {
   return (
     <div className='Datas'>
-      <p>Test</p>
+      <h1>Nicat Əliyev</h1>
+      <p className='birth'>29.03.2004</p>
+      <h1>Haqqımda</h1>
+      <ul>
+        <li>Komandada çalışmağı yaxşı bacarıram, istənilən
+kollektivə asanlıqla uyğunlaşa bilirəm. Əsas
+prinsipim fəaliyyət göstərdiyim sahənin peşəkarı
+olmaq və daim öz üzərimdə çalışmaqdır.
+</li>
+      </ul>
+      <h1>Təhsil</h1>
+      <ul>
+        <li>Azərbaycan Dövlət Neft və Sənaye Universiteti |
+Kompüter mühəndisliyi | 2021 - Davam edir </li>
+      </ul>
+      <h1>İş təcrübəsi</h1>
+      <ul>
+        <li>Azərbaycan Dövlət Neft və Sənaye
+Universitetinin IT departamentində | Help desk</li>
+      </ul>
+      <h1>Proqram biliyi</h1>
+      <ul>
+        <li>SQL</li>
+        <li>Python</li>
+        <li>Oracle DBA</li>
+        <li>C++</li>
+      </ul>
+      <h1>Sertifikatlar</h1>
+      <ul>
+        <li>Programming Essentials in Python. (By Cisco Networking Academy)</li>
+        <li>Certification of Graduation in “Oracle Database” course
+(By Tech Academy) <br /> Verification no.: 2023TAAZ00377</li>
+        <li>"Code for future" lahiyəsinə göstərdiyim texniki dəstək
+üçün Alqoritmika şirkətindən təşəkkürnamə</li>
+      </ul>
     </div>
   )
 }
